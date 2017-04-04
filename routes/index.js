b50526b078473bb57b1dddc13a36f4f1467af72e
Jiaping Zeng <jiaping@gisar.me>
2017-04-03 23:08:03 -0700

    stupify

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var config = require('config')
 var request = require('request')
 var router = express.Router()
 
-var SERVER_URL = (process.env.SERVER_URL) ? (process.env.SERVER_URL) : config.get('serverUrl')
-var CALENDAR_ID = (process.env.CALENDAR_ID) ? (process.env.CALENDAR_ID) : config.get('calendarId')
-var API_KEY = (process.env.API_KEY) ? (process.env.API_KEY) : config.get('apiKey')
+var GOOGLE_CALENDAR_SERVER_URL = (process.env.SERVER_URL) ? (process.env.SERVER_URL) : config.get('serverUrl')
+var GOOGLE_CALENDAR_ID = (process.env.CALENDAR_ID) ? (process.env.CALENDAR_ID) : config.get('calendarId')
+var GOOGLE_API_KEY = (process.env.API_KEY) ? (process.env.API_KEY) : config.get('apiKey')
 
 /* GET home */
 router.get('/', (req, res, next) => {
@@ -18,11 +18,11 @@ router.get('/index', (req, res, next) => {
 
 router.get('/get', (req, res, next) => {
   request({
-    uri: SERVER_URL.replace('calendarId', CALENDAR_ID),
-    qs: { key: API_KEY },
+    uri: GOOGLE_CALENDAR_SERVER_URL.replace('calendarId', GOOGLE_CALENDAR_ID),
+    qs: { key: GOOGLE_API_KEY },
     method: 'GET'
   }, function(error, response, body) {
-    if(!err && res.statusCode == 200) {
+    if(!error && res.statusCode == 200) {
       res.send(body)
     } else {
       console.log('something went wrong')
