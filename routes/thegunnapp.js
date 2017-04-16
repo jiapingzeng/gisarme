c2bafc78c5b00c3f640334e987d69a916218f7e1
Jiaping Zeng <jzeng@outlook.com>
2017-04-15 22:15:18 -0700

    actually works wtf?

--- routes/thegunnapp.js
+++ routes/thegunnapp.js
@@ -1,5 +1,8 @@
-var express = require('express')
 var config = require('config')
+var express = require('express')
+var fs = require('fs')
+var moment = require('moment')
+var path = require('path')
 var request = require('request')
 var router = express.Router()
 
@@ -21,18 +24,69 @@ router.get('/get', (req, res, next) => {
         qs: {
             key: GOOGLE_API_KEY,
             singleEvents: true,
-            orderBy: "startTime"
+            orderBy: "startTime",
+            timeMin: moment().format()
         },
         method: 'GET'
     }, function (error, response, body) {
-        if (!error && res.statusCode == 200) {
+        if (!error && response.statusCode == 200) {
             console.log('successfully retrived calendar')
-            res.render('thegunnapp', { data: JSON.parse(body.trim()) })
+            var data = JSON.parse(body.trim())
+            var calendar = copy(data)
+            calendar.items = []
+            for (var i = 0; i < data.items.length; i++) {
+                var event = data.items[i]
+                if (event.summary.toLowerCase().includes('schedule')) {
+                    calendar.items.push(event)
+                }
+            }
+            res.render('thegunnapp', { data: calendar })
             //res.send(body)
         } else {
             console.log('retriving calendar failed')
         }
     })
 })
+
+router.get('/post', (req, res, next) => {
+    getCalendar(function() {
+        console.log('sending file')
+        //res.sendFile(path.resolve('public/data/thegunnapp.json'))
+        res.sendFile(path.resolve('../data/thegunnapp.json'))
+    }, function() {
+        console.log('error')
+    })    
+})
+
+function copy(o) {
+   var output, v, key;
+   output = Array.isArray(o) ? [] : {};
+   for (key in o) {
+       v = o[key];
+       output[key] = (typeof v === "object") ? copy(v) : v;
+   }
+   return output;
+}
+
+function getCalendar(cb, err) {
+    request({
+        uri: GOOGLE_CALENDAR_SERVER_URL.replace('calendarId', GOOGLE_CALENDAR_ID),
+        qs: {
+            key: GOOGLE_API_KEY,
+            singleEvents: true,
+            orderBy: "startTime",
+            timeMin: moment().format()
+        },
+        method: 'GET'
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('successfully retrived calendar')
+            fs.writeFile(__dirname + '/../public/data/thegunnapp.json', body.trim(), cb)
+        } else {
+            console.log('retriving calendar failed')
+            err()
+        }
+    })
+}
 
 module.exports = router
