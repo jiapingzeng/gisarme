97b9b28eee5bcfd46d29dd0687e9e74671d04892
Jiaping Zeng <jzeng@outlook.com>
2017-05-03 22:35:16 -0700

    dont mind me

--- routes/gnoter.js
+++ routes/gnoter.js
@@ -1,24 +1,83 @@
+var assert = require('assert')
 var config = require('config')
 var express = require('express')
-var pg = require('pg')
 var router = express.Router()
 
-/*
-var connectionString = (process.env.CONNECTION_STRING) ? (process.env.CONNECTION_STRING) : config.get('connectionString')
+/* var connectionString = (process.env.CONNECTION_STRING) ? (process.env.CONNECTION_STRING) : config.get('connectionString')
+var mongo = require('mongodb').MongoClient
 
-pg.connect(connectionString, function(err, client, next) {
-  if (err) {
-    return console.error(':(', err)
-  }
-  client.query('', function(err, result) {
-    next()
-    if (err) {
-      return console.error(':\'(', err)
+mongo.connect(connectionString, function(err, db) {
+  assert.equal(null, err)
+  console.log('connected')
+
+  db.createCollection('gnotes', {
+    validator: {
+      $or: [
+        { key: { $type: "string" } },
+        { content: { $type: "string" } },
+        { passcode: { $type: "string" } }
+      ]
     }
-    console.log(result.rows[0])
-    process.exit(0)
+  })
+
+  insert(db, function() {
+    index(db, function() {
+      db.close()
+    })
   })
 })
+
+var insert = function(db, cb) {
+  var collection = db.collection('docs')
+  collection.insertMany([
+    {a: 1},
+    {a: 2},
+    {a: 3}
+  ], function(err, result) {
+    assert.equal(err, null)
+    assert.equal(3, result.result.n)
+    assert.equal(3, result.ops.length)
+    console.log('inserted')
+    cb(result)
+  })
+}
+
+var find = function(db, cb) {
+  var collection = db.collection('docs')
+  collection.find({'a': 3}).toArray(function(err, docs) {
+    assert.equal(err, null)
+    console.log('found:')
+    console.log(docs)
+    cb(docs)
+  })
+}
+
+var update = function(db, cb) {
+  var collection = db.collection('docs')
+  collection.updateOne({a: 2}, { $set: {b: 1}}, function(err, result) {
+    assert.equal(err, null)
+    assert.equal(1, result.result.n)
+    console.log('updated')
+    cb(result)
+  })
+}
+
+var remove = function(db, cb) {
+  var collection = db.collection('docs')
+  collection.deleteOne({a: 3}, function(err, result) {
+    assert.equal(err, null)
+    assert.equal(1, result.result.n)
+    console.log('removed')
+    cb(result)
+  })
+}
+
+var index = function(db, cb) {
+  db.collection('docs').createIndex({a: 1}, null, function(err, results) {
+    console.log(results)
+    cb()
+  })
+}
 */
 
 router.get('/', (req, res, next) => {
